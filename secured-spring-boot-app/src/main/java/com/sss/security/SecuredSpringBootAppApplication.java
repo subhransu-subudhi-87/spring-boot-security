7c60ef26eadff7c4c5e538a1dcafe93c39f1e1ab
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2021-09-06 16:56:53 +0530

    Spring-Security-App 1st-project-model pushed with Basic Auth Implementation

---
+++ secured-spring-boot-app/src/main/java/com/sss/security/SecuredSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.sss.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ secured-spring-boot-app/src/test/java/com/sss/security/SecuredSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
